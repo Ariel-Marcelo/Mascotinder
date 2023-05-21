f09bd0c4269bfe7ff1d61d857b4746f8eab034c3
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2023-05-20 22:13:29 -0500

    project structure

---
+++ src/main/java/com/mascotinder/petmatchmaker/PetMatchmakerApplication.java
@@ -0,0 +1,13 @@
+package com.mascotinder.petmatchmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMatchmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMatchmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mascotinder/petmatchmaker/PetMatchmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mascotinder.petmatchmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMatchmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
